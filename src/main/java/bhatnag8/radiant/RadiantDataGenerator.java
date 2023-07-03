7469a52e0d98686c3f07598f7d475efbab182274
Arryan Bhatnagar <bhatnag8@purdue.edu>
2023-07-03 11:50:14 -0400

    Initial Commit 3

--- src/main/java/bhatnag8/radiant/RadiantDataGenerator.java
+++ src/main/java/bhatnag8/radiant/RadiantDataGenerator.java
@@ -3,7 +3,7 @@ package bhatnag8.radiant;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class RadiantDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
