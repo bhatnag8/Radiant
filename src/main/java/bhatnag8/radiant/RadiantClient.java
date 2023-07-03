44e6e8bfae0ce007e1d90202e92bc4487323bfed
Arryan Bhatnagar <bhatnag8@purdue.edu>
2023-07-03 11:55:16 -0400

    Initial Commit Final

---
+++ src/main/java/bhatnag8/radiant/RadiantClient.java
@@ -0,0 +1,10 @@
+package bhatnag8.radiant;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadiantClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
